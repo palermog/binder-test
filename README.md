426cb971b5413253e2c0e5a8c3c42601b4120a50
Gregory Palermo <gregoryjpalermo@gmail.com>
2024-04-15 02:48:43 -0400

    testing testing 123

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repository is an example repo to test out the [`holepunch`](https://github.
 
 <!-- badges: start -->
   [![Launch Rstudio Binder](http://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/karthik/binder-test/master?urlpath=rstudio)
-  <!-- badges: end -->
+<!-- badges: end -->
   
   
 
--- Untitled.R
+++ Untitled.R
@@ -1,1 +1,10 @@
 library(biblionetwork)
+
+install.packages("remotes")
+install.packages("desc", "fs", "httr", "rprojroot")
+remotes::install_github("karthik/holepunch", type = "binary")
+
+library(holepunch)
+write_compendium_description(package = "Test please", 
+                             description = "by god i hope this works")
+generate_badge(branch = "main", urlpath = "rstudio")

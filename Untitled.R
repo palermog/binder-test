064142e038a874f70afd92f86617acb7c9903fd7
Gregory Palermo <gregoryjpalermo@gmail.com>
2024-04-15 03:01:23 -0400

    try again

--- Untitled.R
+++ Untitled.R
@@ -1,10 +1,14 @@
 library(biblionetwork)
 
+install.packages("devtools")
 install.packages("remotes")
 install.packages("desc", "fs", "httr", "rprojroot")
 remotes::install_github("karthik/holepunch", type = "binary")
 
 library(holepunch)
+library(devtools)
 write_compendium_description(package = "Test please", 
                              description = "by god i hope this works")
 generate_badge(branch = "main", urlpath = "rstudio")
+
+write_dockerfile(maintainer = "Greg") 
